562bc95f2471ae9e9856442e28cbc3e32b8d02e4
Yuri Kilochek <yuri.kilochek@gmail.com>
2017-03-26 16:41:12 +0300

    Find GLM and GLFW now use PkgConfig.

--- cmake/FindGLFW.cmake
+++ cmake/FindGLFW.cmake
@@ -1,27 +1,2 @@
-include(FindPackageHandleStandardArgs)
-
-find_path(
-    GLFW_INCLUDE_DIRS
-    NAMES
-        GLFW/glfw3.h
-    PATHS
-        /usr/include
-        /usr/local/include
-)
-
-find_library(
-    GLFW_LIBRARIES
-    NAMES
-        glfw
-    PATHS
-        /usr/lib64
-        /usr/lib
-        /usr/local/lib64
-        /usr/local/lib
-)
-
-find_package_handle_standard_args(
-    GLFW
-    DEFAULT_MSG
-    GLFW_INCLUDE_DIRS GLFW_LIBRARIES
-)
+find_package(PkgConfig REQUIRED)
+pkg_search_module(GLFW REQUIRED glfw3)
--- cmake/FindGLM.cmake
+++ cmake/FindGLM.cmake
@@ -1,18 +1,2 @@
-include(FindPackageHandleStandardArgs)
-
-find_path(
-    GLM_INCLUDE_DIRS
-    NAMES
-        glm/glm.hpp
-    PATHS
-        /usr/include
-        /usr/local/include
-)
-
-set(GLM_LIBRARIES)
-
-find_package_handle_standard_args(
-    GLM
-    DEFAULT_MSG
-    GLM_INCLUDE_DIRS
-)
+find_package(PkgConfig REQUIRED)
+pkg_search_module(GLM REQUIRED glm>=0.9.6)
